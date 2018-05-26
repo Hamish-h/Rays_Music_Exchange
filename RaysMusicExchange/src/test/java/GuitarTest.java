b0b0dbb9a479e44ccf1e5550f47bdc9b091f977f
Hanish-H <hamish.h@outlook.com>
2018-05-26 21:18:41 +0100

    GuitarTest competed

--- RaysMusicExchange/src/test/java/GuitarTest.java
+++ RaysMusicExchange/src/test/java/GuitarTest.java
@@ -1,2 +1,26 @@
+import RaysMusicExchange.Instruments.Guitar;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuitarTest {
+
+    Guitar guitar;
+
+    @Before
+    public void before(){
+        guitar = new Guitar("Red", "Gibson" );
+    }
+
+    @Test
+    public void hasColour(){
+        assertEquals("Red", guitar.getColour());
+    }
+
+    @Test
+    public void hasType(){
+        assertEquals("Gibson", guitar.getType());
+    }
+
 }
