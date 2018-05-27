7f81adf05c63c760b48154f71a0124c72b87572c
Hanish-H <hamish.h@outlook.com>
2018-05-27 19:14:45 +0100

    updated tests

--- RaysMusicExchange/src/test/java/PianoTest.java
+++ RaysMusicExchange/src/test/java/PianoTest.java
@@ -19,8 +19,26 @@ public class PianoTest {
     }
 
     @Test
+    public void canDeleteColour(){
+        piano.setColour(null);
+        assertEquals(null, piano.getColour());
+    }
+
+    @Test
+    public void canChangeColour(){
+        piano.setColour("White");
+        assertEquals("White", piano.getColour());
+    }
+
+    @Test
     public void hasType(){
         assertEquals("Upright", piano.getType());
+    }
+
+    @Test
+    public void canChangeType(){
+        piano.setType("Grand");
+        assertEquals("Grand", piano.getType());
     }
 
     @Test
@@ -29,8 +47,21 @@ public class PianoTest {
     }
 
     @Test
+    public void canChangeItem(){
+        piano.setItem("Piano Frame");
+        assertEquals("Piano Frame", piano.getItem());
+    }
+
+
+    @Test
     public void hasBuyPrice(){
         assertEquals(500, piano.getBuyPrice());
+    }
+
+    @Test
+    public void canChangeBuyPrice(){
+        piano.setBuyPrice(600);
+        assertEquals(600, piano.getBuyPrice());
     }
 
     @Test
@@ -38,5 +69,10 @@ public class PianoTest {
         assertEquals(750, piano.getSellPrice());
     }
 
+    @Test
+    public void canChangeSellPrice(){
+        piano.setSellPrice(800);
+        assertEquals(800, piano.getSellPrice());
+    }
 }
 
